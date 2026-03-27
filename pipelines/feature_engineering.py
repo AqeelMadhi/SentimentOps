b0794c4d151ac3202676e46e6352e5c168002b3d
AqeelMadhi <aqeelmadhi25@gmail.com>
2026-03-27 16:30:42 +0200

    feat : implement gold layer transformation for machine learning workloads

---
+++ pipelines/feature_engineering.py
@@ -0,0 +1,42 @@
+import os
+
+import polars as pl
+from dotenv import load_dotenv
+from loguru import logger
+
+
+load_dotenv()
+
+
+def create_sentiment_label(df: pl.DataFrame) -> pl.DataFrame:
+    return df.with_columns(
+        pl.when(pl.col("rating") <= 2)
+        .then(pl.lit("bad"))
+        .when(pl.col("rating") == 3)
+        .then(pl.lit("average"))
+        .otherwise(pl.lit("good"))
+        .alias("sentiment_label")
+    )
+
+
+def engineer():
+    logger.info("Gold transformation started")
+    if not os.getenv("GOLD_PATH"):
+        raise ValueError("Gold path doesnt exist")
+    if not os.getenv("SILVER_PATH"):
+        raise ValueError("Silver Path doesnt exist")
+    try:
+        df = pl.read_delta(os.getenv("SILVER_PATH"))
+        df = create_sentiment_label(df)
+        df.write_delta(
+            os.getenv("GOLD_PATH"),
+            mode="overwrite",
+            delta_write_options={"schema_mode": "overwrite"},
+        )
+        logger.info(f"Successfully wrote {df.height} rows to gold layer")
+    except Exception:
+        logger.exception("Gold transformation failed")
+
+
+if __name__ == "__main__":
+    engineer()
