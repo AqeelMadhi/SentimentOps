f6eb01ddef1728f422b8747ead9044445a9ab3e8
AqeelMadhi <aqeelmadhi25@gmail.com>
2026-04-14 14:35:11 +0200

    feat: implemented parallel downloading of dataset

--- pipelines/ingest.py
+++ pipelines/ingest.py
@@ -1,4 +1,6 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 
 import polars as pl
 from datasets import Dataset, load_dataset
@@ -14,6 +16,19 @@ def prepare_individual_dataset(dataset: Dataset, category: str) -> pl.DataFrame:
     df = df.with_columns(pl.lit(category).alias("category"))
     logger.info(f"Ingested {df.height} rows for category {category}")
     return df
+
+
+def load_and_prepare(category: str, split: str) -> pl.DataFrame:
+    from datasets import disable_progress_bars
+
+    disable_progress_bars()
+    dataset = load_dataset(
+        os.getenv("DATASET_NAME"),
+        category,
+        trust_remote_code=True,
+        split=split,
+    )
+    return prepare_individual_dataset(dataset, category)
 
 
 def ingest_data() -> None:
@@ -37,24 +52,16 @@ def ingest_data() -> None:
 
     try:
         categories = os.getenv("CATEGORIES").split(",")
-        dfs = []
-        for category in categories:
-            dfs.append(
-                prepare_individual_dataset(
-                    dataset=load_dataset(
-                        os.getenv("DATASET_NAME"),
-                        category,
-                        trust_remote_code=True,
-                        split=split,
-                    ),
-                    category=category,
-                )
-            )
+        load_and_prepare_with_split = partial(load_and_prepare, split=split)
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            dfs = list(executor.map(load_and_prepare_with_split, categories))
         dataset_to_be_written = pl.concat(dfs)
         write_deltalake(
             os.getenv("BRONZE_PATH"), dataset_to_be_written.to_arrow(), mode="overwrite"
         )
-        logger.info("Successfully ingested data")
+        logger.info(
+            f"Successfully ingested {dataset_to_be_written.height} rows to Bronze"
+        )
     except Exception:
         logger.exception("Data Ingestion Failed")
         raise
